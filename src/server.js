77cd2ce55c956d6d2678da033c3b85b67f784976
WilsonTestoni <wilson.testoni@jbq.global>
2024-10-01 15:12:00 -0300

    fix: adjust cors origins settings

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://flourishing-elf-04cbc3.netlify.app/"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://flourishing-elf-04cbc3.netlify.app"],
     credentials: true,
   })
 );
